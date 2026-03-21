039b9127bf9b9330b20a996691531c809685181d
ChamiduPrabodya <chamiduworks@gmail.com>
2026-03-21 19:08:16 +0530

    Fix correct frontend path

--- hotel-management-system/frontend/vite.config.js
+++ hotel-management-system/frontend/vite.config.js
@@ -3,8 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/RESTA-FOOD/',   
-
+  base: '/RESTA-FOOD/',
   server: {
     watch: {
       usePolling: true,
